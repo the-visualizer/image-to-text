e13cb0aa01a528f5125247aaa5835329c979433b
The Visualizer <nida09@msn.com>
2017-08-18 20:43:04 +0530

    Converting image to grayscale

---
+++ grayscale.py
@@ -0,0 +1,23 @@
+import os, sys
+from PIL import Image
+
+inFile = ''
+outFile = ''
+
+if len(sys.argv) !=3:
+    print ('Input format error!')
+else:
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+
+im = Image.open(inFile).convert('L')
+ 
+for i in range(im.size[0]):
+    for j in range(im.size[1]):
+        if im.getpixel((i,j)) > 127:
+            im.putpixel((i,j), 255)
+        else:
+            im.putpixel((i,j), 0)
+
+im.show()
+im.save(outFile)                         
