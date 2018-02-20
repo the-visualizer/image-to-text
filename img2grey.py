a7e6efe7df99c351c9c5811bdcd6742aa235e909
Visualizer <nida09@msn.com>
2018-02-20 23:39:31 +0530

    Update img2grey.py

--- img2grey.py
+++ img2grey.py
@@ -3,14 +3,14 @@ import numpy as np
 from scipy import misc
 import matplotlib.pyplot as plt
 
-M = misc.imread('/home/visualizer/Desktop/input4.png')
+M = misc.imread('//path/to/input/image')
 grey = np.zeros(M.shape)
 
 for rownum in range(len(M)):
 	for colnum in range(len(M[0])):
 		grey[rownum][colnum] = np.average(M[rownum][colnum])
 
-misc.imsave('/home/visualizer/Desktop/Output4.png',grey)
+misc.imsave('//path/to/grayscale/image',grey)
 
 #f, axarr = plt.subplots(1,2) # This 
 #axarr[0,0].imshow(M)         # doesn't
